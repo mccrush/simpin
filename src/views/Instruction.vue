0a2cfadd827637cdc33abcaf3017acd762124c24
mccrush <mccrush2027@gmail.com>
2020-02-20 23:09:17 +0700

    Добавляет кнопку возврата к списку

--- src/views/Instruction.vue
+++ src/views/Instruction.vue
@@ -5,6 +5,7 @@
       <hr />
       <p>{{instruction.description}}</p>
       <button class="btn btn-block btn-success" @click="currentstep = 0">Запустить инструкцию</button>
+      <router-link to="/" class="btn btn-block btn-light">К списку инструкций</router-link>
     </div>
     <div v-else-if="instruction && currentstep != null" class="col-12 col-sm-8 col-md-6 col-xl-4">
       <h5 class="d-flex justify-content-between">
@@ -22,6 +23,34 @@
           <button class="btn btn-block btn-light" @click="currentstep++">Далее</button>
         </div>
       </div>
+
+      <!-- <div class="row" v-if="this.currentstep+1 === this.instruction.countsteps">
+        <div class="col-12">
+          <hr />Оцените инструкцию:
+          <br />
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1" v-model="rate" />
+            <label class="form-check-label" for="inlineRadio1">1</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2" v-model="rate" />
+            <label class="form-check-label" for="inlineRadio2">2</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3" v-model="rate" />
+            <label class="form-check-label" for="inlineRadio3">3</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="4" v-model="rate" />
+            <label class="form-check-label" for="inlineRadio4">4</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="5" v-model="rate" />
+            <label class="form-check-label" for="inlineRadio5">5</label>
+          </div>
+          <button class="btn btn-sm btn-success" @click="saveRate">Оценить</button>
+        </div>
+      </div>-->
     </div>
     <div v-else class="col-12 col-sm-8 col-md-6 col-xl-4">
       <h5 class="text-center">Инструкция не найдена</h5>
@@ -35,7 +64,8 @@
 export default {
   data() {
     return {
-      currentstep: null
+      currentstep: null,
+      rate: null
     };
   },
   computed: {
@@ -49,6 +79,9 @@ export default {
         this.currentstep = null;
       }
     }
+  },
+  methods: {
+    saveRate() {}
   }
 };
 </script>
