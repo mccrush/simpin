e5daef2503ff1ea87673764fa474b7e949055f80
mccrush <mccrush2027@gmail.com>
2020-02-20 21:56:03 +0700

    Реализует отображение шагов

--- src/store/index.js
+++ src/store/index.js
@@ -5,56 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    instructions: JSON.parse(localStorage.getItem('instructions') || '[]'),
-    instArr: [
-      {
-        id: '1234556574',
-        title: 'Пример инструкции',
-        countSteps: 3,
-        description: 'Это пример простой, текстовй инструкции. Выполняйте указания для достижения результата.',
-        step: [
-          {
-            title: 'Шаг 1',
-            description: 'Первое, что необходимо сделать - это понять, что любую сложную задачу, можно разбить на простые, элементарные шаги.'
-          },
-          {
-            title: 'Шаг 2',
-            description: 'Научиться выполнять в один момент времени, всего одну задачу. Многозадачность, на самом деле, довольно редкая способность.'
-          },
-          {
-            title: 'Шаг 3',
-            description: 'На этом инструкция окончена. Мы надеимся, что вы достигли результата. Поделитесь этой инструкцией с друзьями.'
-          }
-        ]
-      }
-    ],
-    tecValue: {
-      id: "",
-      title: "",
-      description: "",
-      countSteps: ''
-    },
-    authType: 'login'
+    instructions: JSON.parse(localStorage.getItem('instructions') || '[]')
   },
   mutations: {
-    // getInstArr(state) {
-    //   return state.instArr;
-    // },
-    // setInstArr(state, payload) {
-    //   state.instArr.push(payload);
-    // },
-    // setNewStep(state, payload) {
-    //   state.instArr[payload.index].step.push(payload.content);
-    // },
-    // setTecValue(state, payload) {
-    //   state.tecValue.id = payload.id;
-    //   state.tecValue.title = payload.title;
-    //   state.tecValue.description = payload.description;
-    //   state.tecValue.countSteps = payload.countSteps;
-    // },
-    // setAuthType(state, payload) {
-    //   state.authType = payload.type;
-    // }
     createInstruction(state, instruction) {
       state.instructions.push(instruction);
 
--- src/views/Instruction.vue
+++ src/views/Instruction.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="row justify-content-center">
-    <div v-if="instruction" class="col-12 col-sm-8 col-md-6 col-xl-4">
+    <div v-if="instruction && currentstep === null" class="col-12 col-sm-8 col-md-6 col-xl-4">
       <h5>{{instruction.title}}</h5>
       <hr />
       <p>{{instruction.description}}</p>
-      <button class="btn btn-block btn-success">Запустить инструкцию</button>
+      <button class="btn btn-block btn-success" @click="currentstep = 0">Запустить инструкцию</button>
+    </div>
+    <div v-else-if="instruction && currentstep != null" class="col-12 col-sm-8 col-md-6 col-xl-4">
+      <h5>{{instruction.steps[currentstep].title}}</h5>
+      <hr />
+      <p>{{instruction.steps[currentstep].description}}</p>
+      <button class="btn btn-block btn-light" @click="currentstep++">Далее</button>
     </div>
     <div v-else class="col-12 col-sm-8 col-md-6 col-xl-4">
       <h5 class="text-center">Инструкция не найдена</h5>
@@ -16,9 +22,21 @@
 
 <script>
 export default {
+  data() {
+    return {
+      currentstep: null
+    };
+  },
   computed: {
     instruction() {
       return this.$store.getters.instructionById(+this.$route.params.id);
+    }
+  },
+  watch: {
+    currentstep(val) {
+      if (val === this.instruction.countsteps) {
+        this.currentstep = null;
+      }
     }
   }
 };
